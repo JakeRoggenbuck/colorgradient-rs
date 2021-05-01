6788a226ba6c2451e3c4a47266ae989054f8086d
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-04-30 22:39:02 -0700

    Change types to save memory, and add macro

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,17 @@ struct Channels {
     blue: Vec<i8>,
 }
 
+#[macro_export]
+macro_rules! vec_to_rgb {
+    ($x: expr) => {
+        RGB {
+            r: $x[0] as i8,
+            g: $x[1] as i8,
+            b: $x[2] as i8,
+        }
+    };
+}
+
 fn get_channels(colors: Vec<RGB>) -> Channels {
     // Take a vector of RBG structures and pull the individual colors out and put them in their
     // respective vector in the channels
@@ -39,10 +50,10 @@ fn closest_whole_numbers(x: f32) -> (f32, f32) {
     return (x.floor(), x.ceil());
 }
 
-fn find_y(x: f32, known_x: &Vec<f32>) -> f32 {
+fn find_y(x: f32, known_x: &Vec<i8>) -> f32 {
     if x.fract() == 0.0 {
         let index = x as usize;
-        return known_x[index];
+        return known_x[index] as f32;
     } else {
         let (left_x, right_x) = closest_whole_numbers(x);
 
@@ -52,19 +63,32 @@ fn find_y(x: f32, known_x: &Vec<f32>) -> f32 {
         let slope: f32 = find_slope(
             Point {
                 x: left_x,
-                y: left_y,
+                y: left_y as f32,
             },
             Point {
                 x: right_x,
-                y: right_y,
+                y: right_y as f32,
             },
         );
-        return left_y + (slope * (x - left_x));
+        return left_y as f32 + (slope * (x - left_x));
     }
 }
 
 fn main() {
-    println!("colorgradient");
+    let num = 100;
+
+    let colors = vec![
+        vec_to_rgb![vec![12, 16, 24]],
+        vec_to_rgb![vec![15, 19, 24]],
+        vec_to_rgb![vec![42, 14, 44]],
+    ];
+
+    let channels = get_channels(colors);
+    for channel in [channels.red, channels.green, channels.blue].iter() {
+        for x in 0..num {
+            find_y(x as f32, channel);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -124,7 +148,7 @@ mod tests {
 
     #[test]
     fn find_y_test() {
-        let known_x = vec![12.0, 23.0, 30.0, 49.0, 53.0, 54.0, 41.0, 32.0, 29.0];
+        let known_x = vec![12, 23, 30, 49, 53, 54, 41, 32, 29];
 
         assert_eq!(find_y(0.0, &known_x), 12.0);
         assert_eq!(find_y(1.4, &known_x), 25.8);
